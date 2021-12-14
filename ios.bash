86ce9e11ab9832886776630623ce2efdb2e70995
Simon Schmid <mail@sschmid.com>
2021-12-14 13:06:56 +0100

    Update plugin according to bee 1.0.0 specs

--- ios.bash
+++ ios.bash
@@ -1,33 +1,68 @@
-#!/usr/bin/env bash
+: "${BEE_PROJECT:="Project"}"
+: "${IOS_PROJECT:="${BEE_PROJECT}"}"
+: "${IOS_PROJECT_PATH:="Build/iOS/${BEE_PROJECT}/${IOS_PROJECT}.xcodeproj"}"
+: "${IOS_WORKSPACE_PATH:="Build/iOS/${BEE_PROJECT}/${IOS_PROJECT}.xcworkspace"}"
+: "${IOS_SCHEME:="${IOS_PROJECT}"}"
+: "${IOS_ARCHIVE_PATH:="Build/iOS/${BEE_PROJECT}/${BEE_PROJECT}.xcarchive"}"
+: "${IOS_EXPORT_PATH:="Build/iOS/${BEE_PROJECT}/Export"}"
+: "${IOS_IPA_PATH:="${IOS_EXPORT_PATH}/${IOS_PROJECT}.ipa"}"
+: "${IOS_EXPORT_OPTIONS_PATH:="${BEE_RESOURCES}"/ios/ExportOptions.plist}"
 
-ios::_new() {
-  echo '# ios'
-  echo 'IOS_PROJECT="Build/iOS/${BEE_PROJECT}/Unity-iPhone.xcodeproj"
-IOS_WORKSPACE="Build/iOS/${BEE_PROJECT}/Unity-iPhone.xcworkspace"
-IOS_SCHEME="Unity-iPhone"
-IOS_ARCHIVE="Build/iOS/${BEE_PROJECT}/${BEE_PROJECT}.xcarchive"
-IOS_EXPORT_PATH="Build/iOS/${BEE_PROJECT}/Export"
-IOS_IPA="${IOS_EXPORT_PATH}/Unity-iPhone.ipa"
-IOS_EXPORT_OPTIONS="${BEE_RESOURCES}"/ios/ExportOptions.plist
-# secrets:
-# ios.user
-# ios.password'
+ios::help() {
+  cat << 'EOF'
+Useful commands for iOS development - https://github.com/sschmid/bee-ios
+
+This plugin comes with additional resources.
+Please run 'bee res ios' to copy all required files to your project.
+
+template:
+
+  IOS_PROJECT="${BEE_PROJECT}" # default
+  IOS_PROJECT_PATH="Build/iOS/${BEE_PROJECT}/${IOS_PROJECT}.xcodeproj" # default
+  IOS_WORKSPACE_PATH="Build/iOS/${BEE_PROJECT}/${IOS_PROJECT}.xcworkspace" # default
+  IOS_SCHEME="${IOS_PROJECT}" # default
+  IOS_ARCHIVE_PATH="Build/iOS/${BEE_PROJECT}/${BEE_PROJECT}.xcarchive" # default
+  IOS_EXPORT_PATH="Build/iOS/${BEE_PROJECT}/Export" # default
+  IOS_IPA_PATH="${IOS_EXPORT_PATH}/${IOS_PROJECT}.ipa" # default
+  IOS_EXPORT_OPTIONS_PATH="${BEE_RESOURCES}"/ios/ExportOptions.plist # default
+
+secrets:
+
+  IOS_USER
+  IOS_PASSWORD
+
+usage:
+
+  archive_project     archive Xcode project
+  archive_workspace   archive Xcode workspace
+  export              export Xcode archive
+  upload              upload IPA to TestFlight
+
+bee dependencies:
+
+  none
+
+dependencies:
+
+  Xcode   https://developer.apple.com/xcode
+
+EOF
 }
 
 ios::archive_project() {
   xcodebuild \
-    -project "${IOS_PROJECT}" \
+    -project "${IOS_PROJECT_PATH}" \
     -scheme "${IOS_SCHEME}" \
-    -archivePath "${IOS_ARCHIVE}" \
+    -archivePath "${IOS_ARCHIVE_PATH}" \
     -quiet \
     archive
 }
 
 ios::archive_workspace() {
   xcodebuild \
-    -workspace "${IOS_WORKSPACE}" \
+    -workspace "${IOS_WORKSPACE_PATH}" \
     -scheme "${IOS_SCHEME}" \
-    -archivePath "${IOS_ARCHIVE}" \
+    -archivePath "${IOS_ARCHIVE_PATH}" \
     -quiet \
     archive
 }
@@ -35,19 +70,16 @@ ios::archive_workspace() {
 ios::export() {
   xcodebuild \
     -exportArchive \
-    -archivePath "${IOS_ARCHIVE}" \
+    -archivePath "${IOS_ARCHIVE_PATH}" \
     -exportPath "${IOS_EXPORT_PATH}" \
-    -exportOptionsPlist "${IOS_EXPORT_OPTIONS}" \
+    -exportOptionsPlist "${IOS_EXPORT_OPTIONS_PATH}" \
     -quiet
 }
 
 ios::upload() {
-  local user password
-  user="$(bee::secrets ios.user)"
-  password="$(bee::secrets ios.password)"
   xcrun altool --upload-app \
-    -f "${IOS_IPA}" \
+    -f "${IOS_IPA_PATH}" \
     -t ios \
-    -u "${user}" \
-    -p "${password}"
+    -u "${IOS_USER}" \
+    -p "${IOS_PASSWORD}"
 }
